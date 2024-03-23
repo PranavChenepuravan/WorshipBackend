2e1461398c8d1f9c8c0dd5b32af53ce0d8da7680
Pranav <pranavparathakkad@gmail.com>
2024-03-23 23:25:18 +0530

    Institution Archaeology request function started

---
+++ models/archaeological.js
@@ -0,0 +1,65 @@
+import mongoose from "mongoose";
+
+let archSchema=mongoose.Schema(
+    {
+        institutionId:{
+            type:mongoose.Types.ObjectId,
+            ref:institution
+        },
+        location:{
+            type:String
+        },
+        community:{
+            type:String
+        },
+        name:{
+            type:String
+        },
+        phone:{
+            type:String
+        },
+        email:{
+            type:String,
+            unique:true
+        },
+        date:{
+            type:String
+        },
+        wealthid:{
+            type:String
+        },
+        wealthname:{
+            type:String
+        },
+        type:{
+            type:String
+        },
+        size:{
+            type:String
+        },
+        weight:{
+            type:String
+        },
+        eraofmanufacture:{
+            type:String
+        },
+        madein:{
+            type:String
+        },
+        material:{
+            type:String
+        },
+        antiquevalue:{
+            type:String
+        },
+        heritage:{
+            type:String
+        }
+
+
+    }
+)
+
+let Archaeological=mongoose.model('archaeological',archSchema)
+
+export default Archaeological

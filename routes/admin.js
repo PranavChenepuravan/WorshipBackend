4d3c9f1b23ab94025ca87ee92e46afd26a5bcb87
Pranav <pranavparathakkad@gmail.com>
2024-04-02 10:33:47 +0530

    All functions of Admin module except report are started

--- routes/admin.js
+++ routes/admin.js
@@ -3,13 +3,6 @@ import User from '../models/user.js'
 
 const router=express()
 
-router.get('/viewinstprofile/',async (req,res)=>{
-    console.log();
-    let response=await User.find({userType:"institution"})
-    console.log(response);
-    res.json(response)
-})
-
 router.put('/manageUser/:id',async(req,res)=>{
     let id=req.params.id
     console.log(id);
@@ -17,5 +10,32 @@ router.put('/manageUser/:id',async(req,res)=>{
     let response=await User.findByidAndUpdate(id,req.body)
 })
 
+router.get('/viewinstprofile/',async (req,res)=>{
+    console.log();
+    let response=await User.find({userType:"institution"})
+    console.log(response);
+    res.json(response)
+})
+
+router.get('/viewincomeprofile/',async (req,res)=>{
+    console.log();
+    let response=await User.find({userType:"incometax"})
+    console.log(response);
+    res.json(response)
+})
+
+router.get('/viewarchprofile/',async (req,res)=>{
+    console.log();
+    let response=await User.find({userType:"archaeology"})
+    console.log(response);
+    res.json(response)
+})
+
+router.get('/viewpilgprofile/',async (req,res)=>{
+    console.log();
+    let response=await User.find({userType:"pilgrim"})
+    console.log(response);
+    res.json(response)
+})
 
 export default router
